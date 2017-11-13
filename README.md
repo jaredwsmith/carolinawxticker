c8a30eed4a13764228b2ac0a4272881d1358f60b
Jared Smith <jared@jaredwsmith.com>
2017-11-12 23:50:32 -0500

    It works...sort of

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # carolinawxticker
 Ticker for Carolina Weather Group on-air use.
+
+Based on a CodePen by Lewis Carey (https://codepen.io/lewismcarey/pen/GJZVoG)
---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+/* 
+ * CarolinaWXticker
+ */
+
+const parser = require('rss-parser');
+
+parser.parseURL('http://www.rssmix.com/u/8262815/rss.xml', function (err, parsed) {
+    
+})
